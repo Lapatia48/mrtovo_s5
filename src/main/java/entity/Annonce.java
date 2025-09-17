c32834e4e996137b5856cbc7fe1ece0238bea339
Lapatia48 <ravelojaona48@gmail.com>
2025-09-17 15:49:32 +0300

    a

---
+++ src/main/java/entity/Annonce.java
@@ -0,0 +1,88 @@
+package entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "annonce")
+public class Annonce {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "id_departement")
+    private Integer idDepartement;
+
+    @Column(name = "titre", length = 150)
+    private String titre;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "date_publication")
+    private LocalDate datePublication;
+
+    @Column(name = "nb_personne_utile")
+    private Integer nbPersonneUtile;
+
+    @Column(name = "salaire")
+    private Integer salaire;
+
+    // Getters et Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getIdDepartement() {
+        return idDepartement;
+    }
+
+    public void setIdDepartement(Integer idDepartement) {
+        this.idDepartement = idDepartement;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDatePublication() {
+        return datePublication;
+    }
+
+    public void setDatePublication(LocalDate datePublication) {
+        this.datePublication = datePublication;
+    }
+
+    public Integer getNbPersonneUtile() {
+        return nbPersonneUtile;
+    }
+
+    public void setNbPersonneUtile(Integer nbPersonneUtile) {
+        this.nbPersonneUtile = nbPersonneUtile;
+    }
+
+    public Integer getSalaire() {
+        return salaire;
+    }
+
+    public void setSalaire(Integer salaire) {
+        this.salaire = salaire;
+    }
+}
---
+++ target/projet_MrTovo/WEB-INF/views/index.jsp
@@ -0,0 +1,17 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="jakarta.tags.core" prefix="c" %>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Bienvenue sur la page d'accueil</h1>
+    <a href="${pageContext.request.contextPath}/formLogCandidat">Login candidat</a><br>
+    <a href="${pageContext.request.contextPath}/formLogRh">Login Ressource humaine</a><br>
+    <a href="${pageContext.request.contextPath}/formLogManager">Login Manager</a><br>
+    <a href="${pageContext.request.contextPath}/formLogAdmin">Login Admin</a><br>
+</body>
+</html>
