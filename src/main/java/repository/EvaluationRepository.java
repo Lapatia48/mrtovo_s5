38ca2f79954a6a86d3403abfb8dfd2ba4f98f343
Andie <andiemiarintsoa05@gmail.com>
2025-11-29 17:33:28 +0300

    Create EvaluationRepository.java
    methode

---
+++ src/main/java/repository/EvaluationRepository.java
@@ -0,0 +1,16 @@
+package repository;
+
+public package repository;
+
+import entity.Evaluation;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EvaluationRepository extends JpaRepository<Evaluation, Long> {
+    List<Evaluation> findByEmployeIdAndMoisAndAnnee(Long employeId, String mois, int annee);
+}
+ {
+    
+}
